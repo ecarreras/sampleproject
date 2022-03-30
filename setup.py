a2ce96d246216db3654d4716eb546cb6ca0c7223
Eduard Carrerars <ecarreras@gisce.net>
2022-03-31 01:41:50 +0200

    Change name and version

--- setup.py
+++ setup.py
@@ -32,14 +32,14 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name="sampleproject",  # Required
+    name="sample-ecarreras",  # Required
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
     #
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/guides/single-sourcing-package-version/
-    version="2.0.0",  # Required
+    version="0.0.1",  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
